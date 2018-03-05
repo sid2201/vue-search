6416f5022ee8b6633b2e553daa9f2c1303fc64b2
Siddharth Srinivasan <Sid@Sid-JARVIS.local>
2018-03-05 16:40:13 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <SearchBox></SearchBox>
+  </div>
+</template>
+
+<script>
+import SearchBox from './components/SearchBox'
+
+export default {
+  name: 'App',
+  components: {
+    SearchBox
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/SearchBox.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <input type="text" v-model="searchText" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBox',
+  data () {
+    return {
+      searchText: 'Hello, World'
+    }
+  }
+}
+</script>
